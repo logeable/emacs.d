d1a576f28749be474e83403ec194add7812806af
logeable <logeable@gmail.com>
2023-07-13 10:53:23 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,14 @@
+(global-linum-mode 1)
+
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-init-file)
+(global-set-key (kbd "C-<f5>") 'load-file)    
+
+(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+
