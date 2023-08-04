85e80d184c980c81206020242e49400cfbb79e3d
logeable <logeable@gmail.com>
2023-08-05 02:50:45 +0800

    org配置

--- init.el
+++ init.el
@@ -1,14 +1,4 @@
-(global-linum-mode 1)
-
-(defun open-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-(global-set-key (kbd "<f2>") 'open-init-file)
-(global-set-key (kbd "C-<f5>") 'load-file)    
-
-(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
-                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-(package-initialize)
-
-
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
